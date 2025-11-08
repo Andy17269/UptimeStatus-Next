c8b52b78e0cb073edd01229fc23dac4236b726a5
Andy17269 <wenlei660185@gmail.com>
2025-11-08 18:55:34 +0800

    push

---
+++ public/config.js
@@ -0,0 +1,25 @@
+window.Config = {
+
+  // 【自定义项】显示标题
+  SiteName: 'Uptime Status',
+
+  // 【自定义项】UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only Key
+  ApiKeys: [
+    'm801079944-11c1a25b1037e1b06fd7a1ca',
+  ],
+
+  // 日志显示天数（免费版API最多显示90天）
+  CountDays: 90,
+
+  // 是否显示检测站点的链接（UptimeRobot站内的探测链接）
+  ShowLink: false,
+
+  // 【自定义项】导航栏菜单
+  Navi: [
+    {
+      text: 'Blog',
+      url: 'https://wenlei.top/?from=wcuts'
+    },
+  ],
+};
---
+++ src/components/app.js
@@ -0,0 +1,34 @@
+import { useMemo } from 'react';
+import Link from './link';
+import Header from './header';
+import UptimeRobot from './uptimerobot';
+import Package from '../../package.json';
+
+function App() {
+
+  const apikeys = useMemo(() => {
+    const { ApiKeys } = window.Config;
+    if (Array.isArray(ApiKeys)) return ApiKeys;
+    if (typeof ApiKeys === 'string') return [ApiKeys];
+    return [];
+  }, []);
+  // 自定义页脚（请保留Github项目链接）
+  return (
+    <>
+      <Header />
+      <div className='container'>
+        <div id='uptime'>
+          {apikeys.map((key) => (
+            <UptimeRobot key={key} apikey={key} />
+          ))}
+        </div>
+        <div id='footer'>
+          <p>&copy; 2025 <Link to='https://github.com/WenCenter/UptimeStatus-Next' text='Uptime Status Next' /></p>
+          <p>在 <Link to='https://github.com/WenCenter/UptimeStatus-Next' text='Github'/>↗️ 上查看此项目</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
